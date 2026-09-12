1df140c85de3991ab763eb60fd45157a9ddd7bc5
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-12 15:44:02 +0530

    add delete patient

--- Projects_Over_100_Lines/Hospital_Management_System.py
+++ Projects_Over_100_Lines/Hospital_Management_System.py
@@ -21,7 +21,7 @@ def add_patients():
 add_patients()   
 print(patients)
 
-
+print("\nview patient")
 def view_patient():
     patient_id = input("Enter patient Id:")
     
@@ -42,7 +42,7 @@ def view_patient():
         
 view_patient()
 
-
+print("\nsearch patient")
 def search_patient():
     patient_id = input("Enter patient Id:")
     
@@ -65,6 +65,7 @@ search_patient()
         
 doctors = {}
 
+print("\nAdd doctor")
 def add_doctor():
     doctor_id = input("Enter Doctor Id:")
     doctor_name = input("Enter Doctor Name:")
@@ -79,4 +80,75 @@ def add_doctor():
     print("\n Doctor Added Successfully!")
     print("---------------------------------")
     
+add_doctor()
+print(doctors)
+
+
+appointments = {}
+
+print("\n Book Appointment")
+def book_appointment():
+    patient_id = input("Enter Patient Id:")
     
+    if patient_id not in patients:
+        print("\nPatient Not Found!")
+        return
+    
+    doctor_id = input("Enter Doctor Id:")
+    
+    if doctor_id not in doctors:
+        print("\n Doctor Not Found!")
+        return
+    
+    appointment_date = input("Enter Appointment Date:")
+    
+    appointments[patient_id] = {
+        "doctor_id" : doctor_id,
+        "appointment_date" :appointment_date
+    }
+        
+    print("\n Appointment Booked Successfully!")
+    print("--------------------------------------")
+    
+book_appointment()   
+
+
+print("\n Hospital Bill")
+
+def generate_bill():
+    patient_id = input("Enter patient Id:")
+    
+    if patient_id not in patients:
+        print("\n Patient Not Found!")
+        return
+    
+    consultation_fee = float(input("Enter Consultation Fee:"))
+    medicine_charges = float(input("Enter Medicine Charges:"))
+    
+    total_bill = consultation_fee + medicine_charges
+    
+    print("\n------HOSPITAL BILL------")
+    print(f"Patient Id : {patient_id}")
+    print(f"Patient Name : {patients[patient_id]['patient_name']}")
+    print(f"Consultation Fee : {consultation_fee}")
+    print(f"Medicine Charges : {medicine_charges}")
+    print(f"Total Bill : {total_bill}")
+    print("-----------------------------------------")
+    
+generate_bill()
+
+print("\n Delete Patient")
+
+def delete_patient():
+    patient_id = input("Enter Patient Id to delete:")
+    
+    if patient_id in patients:
+        del patients[patient_id]
+        
+        print("\n Patient Deleted Successfully!")
+        print("----------------------------------")
+        
+    else:
+        print("\nPatient Not Found!")    
+        
+delete_patient()
