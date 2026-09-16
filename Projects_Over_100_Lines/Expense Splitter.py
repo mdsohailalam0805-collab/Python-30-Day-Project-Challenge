66fc27d26d14f2288f61f1aba4e5458ebcee465b
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-16 12:55:49 +0530

    finally project completed

--- Projects_Over_100_Lines/Expense Splitter.py
+++ Projects_Over_100_Lines/Expense Splitter.py
@@ -24,7 +24,7 @@ def add_expenses():
     
     print("\n Expense Added Successfully!")
     print(f"Name : {name}")
-    print(f"amount : {amount}")
+    print(f"Amount : {amount}")
     print("-------------------------------")
     
 def view_expenses():
@@ -56,6 +56,7 @@ def calculate_split():
     
     
 def view_settlement():
+    
     if not people:
         print("\n No People Available!")
         return
@@ -74,9 +75,48 @@ def view_settlement():
             print(f"{name} should receive {difference:.2f}")       
             
         elif difference < 0:
-            print(f"{name} should pay {abs(difference):.2f)}")
+            print(f"{name} should pay {abs(difference):.2f}")
             
         else:
             print(f"{name} is settled.")
             
     print("------------------------------------------")
+    
+def main_menu():
+
+    while True:
+
+        print("\n===== EXPENSE SPLITTER =====")
+        print("1. Add Person")
+        print("2. Add Expense")
+        print("3. View Expenses")
+        print("4. Calculate Split")
+        print("5. View Settlement")
+        print("6. Exit")
+
+        choice = input("Enter Your Choice: ")
+
+        if choice == "1":
+            add_person()
+
+        elif choice == "2":
+            add_expenses()
+
+        elif choice == "3":
+            view_expenses()
+
+        elif choice == "4":
+            calculate_split()
+
+        elif choice == "5":
+            view_settlement()
+
+        elif choice == "6":
+            print("\nThank You For Using Expense Splitter!")
+            break
+
+        else:
+            print("\nInvalid Choice! Please Try Again!")
+
+
+main_menu()
