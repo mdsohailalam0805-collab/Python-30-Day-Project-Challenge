1e919a5532be679fc4e8262d59e2e4c99ee7576b
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-16 12:46:12 +0530

    add new features

--- Projects_Over_100_Lines/Expense Splitter.py
+++ Projects_Over_100_Lines/Expense Splitter.py
@@ -53,3 +53,30 @@ def calculate_split():
     print(f"Total People : {len(people)}")
     print(f"Per Person Share : {per_person_share}")
     print("----------------------------------------")
+    
+    
+def view_settlement():
+    if not people:
+        print("\n No People Available!")
+        return
+    
+    total_expense = sum(expenses.values())
+    per_person_share = total_expense / len(people)
+    
+    print("\n =====SETTELEMENT=====")
+    
+    for name in people:
+        
+        paid_amount = expenses.get(name, 0) 
+        difference = paid_amount - per_person_share
+        
+        if difference > 0:
+            print(f"{name} should receive {difference:.2f}")       
+            
+        elif difference < 0:
+            print(f"{name} should pay {abs(difference):.2f)}")
+            
+        else:
+            print(f"{name} is settled.")
+            
+    print("------------------------------------------")
