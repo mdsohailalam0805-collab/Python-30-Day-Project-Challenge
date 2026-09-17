9e9b21bc86ca2759652cd388fb9cf6051b5787a4
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-17 14:17:46 +0530

    create new project

---
+++ Projects_Over_100_Lines/Bank Transaction Analyzer.py
@@ -0,0 +1,25 @@
+transactions = []
+
+def add_transaction():
+    
+    transaction_type = input("Enter transaction type (Income/Expense):")
+    category = input("Enter Category:")
+    amount = float(input("Enter amount:"))
+    
+    transaction = {
+        "type" : transaction_type,
+        "category" : category,
+        "amount" : amount
+    }
+    
+    transactions.append(transaction)
+    
+    print("\n Transaction Added Successfully!")
+    
+    print(f"Type : {transaction_type}")
+    print(f"Category : {category}")
+    print(f"Amount : {amount:.2f}")
+    print("-----------------------------")
+    
+    
+    
