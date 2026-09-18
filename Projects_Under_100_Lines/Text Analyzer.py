7d77575f4b3dc1cf5d4d71fe63a0d1e957f1a278
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-18 12:25:00 +0530

    add new features

--- Projects_Under_100_Lines/Text Analyzer.py
+++ Projects_Under_100_Lines/Text Analyzer.py
@@ -9,3 +9,32 @@ print(f"Total Character : {character_count}")
 words = text.split()
 words_count =len(words)
 print(f"Total Words : {words_count}")
+
+
+vowels = "aeiou"
+vowel_count = 0
+
+for char in text.lower():
+    
+    if char in vowels:
+        vowel_count += 1
+        
+print(f"Total Vowels : {vowel_count}")
+
+
+word_frequency = {}
+
+for word in words:
+    word = word.lower()
+    
+    if word in word_frequency:
+        word_frequency[word] += 1
+         
+    else:
+        word_frequency[word] = 1
+        
+print("\n =====WORD FREQUENCY=====")
+
+for word, count in word_frequency.items():
+    print(f"{word} : {count}")
+        
