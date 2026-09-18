c899c9ba405e462a5b0564dcda34196f6ef2fbb6
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-18 12:08:34 +0530

    add new features

--- Projects_Under_100_Lines/Text Analyzer.py
+++ Projects_Under_100_Lines/Text Analyzer.py
@@ -2,3 +2,10 @@ text = input("Enter your Text:")
 
 print("\nText Received Successfully!")
 print(f"Text : {text}")
+
+character_count = len(text)
+print(f"Total Character : {character_count}")
+
+words = text.split()
+words_count =len(words)
+print(f"Total Words : {words_count}")
