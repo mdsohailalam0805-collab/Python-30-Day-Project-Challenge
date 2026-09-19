f86235f2c3ed5e38d1b2c3bd3a6223f6a704959a
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-20 02:48:04 +0530

    add new features

--- Projects_Under_100_Lines/Rock Paper Scissors.py
+++ Projects_Under_100_Lines/Rock Paper Scissors.py
@@ -19,3 +19,15 @@ elif user_choice == 3:
     user_choice = "Scissors"
     
 print(f" You Chose : {user_choice}")
+
+# winnig condition
+if computer_choice == user_choice:
+    print("It's a Draw!")
+    
+elif(user_choice == "Rock" and computer_choice == "Scissors") or \
+    (user_choice == "Scissors" and computer_choice == "Paper") or \
+    (user_choice == "Paper" and computer_choice == "Rock"):
+    print("You Win!")
+    
+else:
+    print("Computer Win!")
