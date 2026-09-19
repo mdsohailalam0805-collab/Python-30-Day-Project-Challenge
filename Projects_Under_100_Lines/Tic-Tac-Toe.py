6fc1f75512b5ae15d03f7a568b7988ac30d6b74f
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-20 02:23:27 +0530

    complete project tic tac toe

--- Projects_Under_100_Lines/Tic-Tac-Toe.py
+++ Projects_Under_100_Lines/Tic-Tac-Toe.py
@@ -1,72 +1,75 @@
 
-board =[" ", " ", " ",
-        " ", " ", " ",
-        " ", " ", " "]
-
-# print(board[0], "|", board[1], "|", board[2])
-# print("----------")
-
-# print(board[3], "|", board[4], "|", board[5])
-# print("----------")
-
-# print(board[6], "|", board[7], "|", board[8])
-
-
-player = "X"
-winner = False
-
-while True:
+def play_game():
     
-    position = int(input(f"Player {player}, Enter position (1-9):"))
-    
-    if position < 1 or position > 9:
-        print("Invalid Position! Enter Between 1 and 9.")
-        continue
-    
-    if board[position-1] == " ":
-        board [position-1] = player
-    
-    else:
-        print("Position Allready Occupied!")
-        continue
-    
-    # Updated board dikhana
-    
-    print(board[0], "|", board[1], "|", board[2])
-    print("----------")
+    board =[" ", " ", " ",
+            " ", " ", " ",
+            " ", " ", " "]
 
-    print(board[3], "|", board[4], "|", board[5])
-    print("----------")
+    # print(board[0], "|", board[1], "|", board[2])
+    # print("----------")
 
-    print(board[6], "|", board[7], "|", board[8])
+    # print(board[3], "|", board[4], "|", board[5])
+    # print("----------")
+
+    # print(board[6], "|", board[7], "|", board[8])
 
 
-    # Winning condition
+    player = "X"
+    winner = False
+
+    while True:
     
-    if (board[0] == board[1] == board[2] != " " or
-        board[3] == board[4] == board[5] != " " or
-        board[6] == board[7] == board[8] != " " or
-        board[0] == board[3] == board[6] != " " or
-        board[1] == board[4] == board[7] != " " or
-        board[2] == board[5] == board[8] != " " or
-        board[0] == board[4] == board[8] != " " or
-        board[2] == board[4] == board[6] != " "):
+        position = int(input(f"Player {player}, Enter position (1-9):"))
+    
+        if position < 1 or position > 9:
+            print("Invalid Position! Enter Between 1 and 9.")
+            continue
+    
+        if board[position-1] == " ":
+            board [position-1] = player
+    
+        else:
+            print("Position Allready Occupied!")
+            continue
+    
+        # Updated board dikhana
+    
+        print(board[0], "|", board[1], "|", board[2])
+        print("----------")
+
+        print(board[3], "|", board[4], "|", board[5])
+        print("----------")
+
+        print(board[6], "|", board[7], "|", board[8])
+
+
+        # Winning condition
+    
+        if (board[0] == board[1] == board[2] != " " or
+            board[3] == board[4] == board[5] != " " or
+            board[6] == board[7] == board[8] != " " or
+            board[0] == board[3] == board[6] != " " or
+            board[1] == board[4] == board[7] != " " or
+            board[2] == board[5] == board[8] != " " or
+            board[0] == board[4] == board[8] != " " or
+            board[2] == board[4] == board[6] != " "):
         
-        winner = True
-        print(f"\n Player {player} Winner!")
-        break
+            winner = True
+            print(f"\n Player {player} Winner!")
+            break
     
     
-    # Draw condition
+        # Draw condition
 
-    if " " not in board:
-        print("\n Game Draw!")
-        break
+        if " " not in board:
+            print("\n Game Draw!")
+            break
 
-    # player switch
-    if player == "X":
-        player = "O"
+        # player switch
+        if player == "X":
+            player = "O"
         
-    else:
-        player = "X"
-
+        else:
+            player = "X"
+            
+play_game()
