123561d37418fe6f4ee2632c75da7b833088682c
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-20 02:39:11 +0530

    add new things

--- Projects_Under_100_Lines/Rock Paper Scissors.py
+++ Projects_Under_100_Lines/Rock Paper Scissors.py
@@ -6,3 +6,16 @@ choices = ["Rock" , "Paper" , "scissors"]
 computer_choice = random.choice(choices)
 
 print(f"Computer Chose : {computer_choice}")
+
+user_choice = int(input("Enter your Choice (1-3):"))
+
+if user_choice == 1:
+    user_choice = "Rock"
+    
+elif user_choice == 2:
+    user_choice = "Paper"
+    
+elif user_choice == 3:
+    user_choice = "Scissors"
+    
+print(f" You Chose : {user_choice}")
