e602991cfdd7cb5ba6371e390216f215e1036874
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-19 13:07:47 +0530

    add new features

--- Projects_Under_100_Lines/Tic-Tac-Toe.py
+++ Projects_Under_100_Lines/Tic-Tac-Toe.py
@@ -3,10 +3,37 @@ board =[" ", " ", " ",
         " ", " ", " ",
         " ", " ", " "]
 
-print(board[0], "|", board[1], "|", board[2])
-print("----------")
+# print(board[0], "|", board[1], "|", board[2])
+# print("----------")
 
-print(board[3], "|", board[4], "|", board[5])
-print("----------")
+# print(board[3], "|", board[4], "|", board[5])
+# print("----------")
 
-print(board[6], "|", board[7], "|", board[8])
+# print(board[6], "|", board[7], "|", board[8])
+
+
+player = "X"
+
+for turn in range (9):
+    
+    position = int(input(f"Player {player}, Enter position (1-9):"))
+    
+    board [position-1] = player
+    
+    
+    # Updated board dikhana
+    print(board[0], "|", board[1], "|", board[2])
+    print("----------")
+
+    print(board[3], "|", board[4], "|", board[5])
+    print("----------")
+
+    print(board[6], "|", board[7], "|", board[8])
+
+    
+    if player == "X":
+        player = "O"
+        
+    else:
+        player = "X"
+
