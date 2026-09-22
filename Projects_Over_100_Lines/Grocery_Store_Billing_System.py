a0271d255e3cb4c9a9dfae1d5d19140e174def0c
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-09-22 14:54:47 +0530

    create new project

---
+++ Projects_Over_100_Lines/Grocery_Store_Billing_System.py
@@ -0,0 +1,10 @@
+products = {
+    1: {"name" : "Rice", "price" : 60},
+    2: {"name" : "Wheat", "price" : 40},
+    3: {"name" : "Sugar", "price" : 80},
+    4: {"name" : "Milk", "price" : 30},
+    5: {"name" : "Bread", "price" : 40},
+    6: {"name" : "Eggs", "price" : 8},
+    7: {"name" : "Oil", "price" : 120},
+    8: {"name" : "Biscuits", "price" : 50}
+}
